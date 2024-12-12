632b01a366c38c2b675055592d48146e02644b5c
Muvin-Mohammad <103063205+Muvin-Mohammad@users.noreply.github.com>
2024-12-12 23:50:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ async function run() {
 
 
 
-    // await client.connect();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
