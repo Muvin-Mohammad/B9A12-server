b5a4a500f8412931c974b7da5206f02f1605b153
Muvin-Mohammad <103063205+Muvin-Mohammad@users.noreply.github.com>
2024-12-12 23:49:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 8000
 
 
-// middleware
+
 const corsOptions = {
   origin: ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
