a5e41718ee29a6ef4a225d64260284d00350bd3c
Muvin-Mohammad <103063205+Muvin-Mohammad@users.noreply.github.com>
2024-12-12 23:49:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const verifyToken = async (req, res, next) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gbneo1a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
